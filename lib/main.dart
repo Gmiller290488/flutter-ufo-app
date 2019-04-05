f63b33a8ceca4da70d51c0c00028b6dc28264967
gmiller290488 <gmiller290488>
2019-04-06 00:41:18 +0100

    List view now is built with song widgets that contain a button - tapping abutton navigates to new (blank) screen

--- lib/main.dart
+++ lib/main.dart
@@ -48,20 +48,92 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  onButtonClicked(title) {
+    setState(() {
+      print("Button clicked");
+      print(title);
+    });
+  }
+
   Widget _buildItem(Song song) {
+
+    List<String> _tenor = song.tenorMusic;
+    List<String> _second = song.secondsMusic;
     return new Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListTile(
-            title: Text(song.title, style: TextStyle(fontSize: 24.0)),
-            subtitle: Text(song.artist),
-            onTap: () async {
+      padding: const EdgeInsets.all(16.0),
+      child: Center(
+        child: ExpansionTile(
+          title: Text(song.title, style: TextStyle(fontSize: 24.0),textAlign: TextAlign.start),
+          children: [
+            Column(
+                children: <Widget>[
+                  Text(song.artist),
 
-              if (await canLaunch(song.url)) {
-
-                launch(song.url);
-              };
-            }
-        )
+                  MaterialButton(
+                    color: Colors.green,
+                    onPressed: () async {
+                      if (await canLaunch(song.url)) {
+                        launch(song.url);
+                      };
+                    },
+                    child: Text("Open in Youtube"),
+                  ),
+                  Column(
+                    children: _tenor.map((tenorMusic) => MusicButton(title: tenorMusic)).toList(),
+//                    children: _tenor.map((tenorMusic) => Text(tenorMusic)).toList(),toList
+                  ),
+                  Column(
+                    children: _second.map((secondMusic) => Text(secondMusic)).toList(),
+                  ),
+                ]
+            ),
+          ],
+//            }
+        ),
+      ),
     );
   }
 }
+
+class MusicButton extends StatelessWidget {
+
+  final String title;
+  final Function(String) onPressed;
+
+  const MusicButton({ this.title, this.onPressed });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child:
+          Text(this.title),
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SecondRoute()),
+        );
+      }
+    );
+  }
+}
+
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("second route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("go back!"),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/src/song.dart
+++ lib/src/song.dart
@@ -3,23 +3,33 @@ class Song {
   final String title;
   final String url;
   final String artist;
+  final List<String> tenorMusic;
+  final List<String> secondsMusic;
+
 
   const Song(
-  {
-    this.title,
-  this.url,
-  this.artist});
-  }
+      {
+        this.title,
+        this.url,
+        this.artist,
+        this.tenorMusic,
+        this.secondsMusic});
+}
 
-  final songs = [
-    new Song(
+final songs = [
+  new Song(
       title:"Hulk",
       url: "https://www.youtube.com/watch?v=zHb1vXpffhI",
-      artist: "Blaxx"
-    ),
-    new Song(
+      artist: "Blaxx",
+      tenorMusic: ["hulk-tenor1.png", "hulk-tenor2.png"],
+      secondsMusic: ["hulk-seconds"]
+
+  ),
+  new Song(
       title: "Inside The Festival",
       url: "https://www.youtube.com/watch?v=NnOVm4E9VaU",
-      artist: "Ultimate Rejects"
-    )
-  ];
+      artist: "Ultimate Rejects",
+    tenorMusic: ["ITF-tenor.png"],
+    secondsMusic: []
+  )
+];
