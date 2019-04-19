519af099ac2a351c4f062727895b9813c1f9a43e
Gareth Miller <gmiller290488@gmail.com>
2019-04-19 21:31:00 +0100

    Changed the UI

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,7 @@ class _MyHomePageState extends State {
     final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
 
     return parsed.map<Song>((json) => Song.fromJson(json)).toList();
+
   }
 
   initState() {
@@ -71,52 +72,45 @@ class _MyHomePageState extends State {
   @override
   build(context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("UFO"),
-        ),
-        body: ListView.builder(
-          itemCount: songs.length,
-          itemBuilder: (context, index) {
-            return _buildItem(songs[index]);
-          },
-        )
-    );
-    }
-  }
-
-  Widget _buildItem(Song song) {
-
-    List<MusicNote> _tenor = song.notes;
-    return new Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Center(
-        child: ExpansionTile(
-          title: Text(song.songName, style: TextStyle(fontSize: 24.0),textAlign: TextAlign.start),
-          children: [
-            Column(
-                children: <Widget>[
-                  Text(song.artist, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-
-                  MaterialButton(
-                    color: Colors.green,
-                    onPressed: () async {
-                      if (await canLaunch(song.youtubeUrl)) {
-                        launch(song.youtubeUrl);
-                      };
-                    },
-                    child: Text("Open in Youtube"),
-                  ),
-                  Column(
-                    children: _tenor.map((tenorMusic) => MusicButton(musicNote: tenorMusic)).toList(),
-                  ),
-                ]
+      appBar: AppBar(
+        title: Text("UFO"),
+      ),
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              // Add one stop for each color. Stops should increase from 0 to 1
+              stops: [0.1, 0.5, 0.7, 0.9],
+              colors: [
+                // Colors are easy thanks to Flutter's Colors class.
+                Colors.lightBlue[900],
+                Colors.lightBlue[700],
+                Colors.lightBlue[500],
+                Colors.lightBlue[200],
+              ],
             ),
-
-           ],
-        ),
+          ),
+          child:
+          songs == null ? Center(child: CircularProgressIndicator()) : ListView.builder(
+            itemCount: songs.length,
+            itemBuilder: (context, index) {
+              return _buildItem(songs[index]);
+            },
+          )
       ),
     );
   }
+}
+
+Widget _buildItem(Song song) {
+
+  return new Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Center(
+        child: CustomExpansionTile(song: song)
+    ),
+  );
+}
 
 
 class MusicButton extends StatelessWidget {
@@ -128,16 +122,16 @@ class MusicButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      color: Colors.grey,
-      child:
-          Text(musicNote.name.toString()),
-      onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => SecondRoute(imageName: musicNote.imgUrl)),
-        );
-      }
+    return OutlineButton(
+        color: Colors.transparent,
+        child:
+        Text(musicNote.name.toString(), style: TextStyle(color: Colors.white70)),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondRoute(imageName: musicNote.imgUrl)),
+          );
+        }
     );
   }
 }
@@ -154,7 +148,83 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text(this.imageName),
       ),
-      body: Image.network(imageName)
-      );
+      body: Card(
+        color: Colors.lightBlue,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Image.network(imageName)
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class CustomExpansionTile extends StatefulWidget {
+
+  final Song song;
+  const CustomExpansionTile({Key key, this.song}): super(key: key);
+
+  @override
+  State createState() => CustomExpansionTileState();
+}
+
+class CustomExpansionTileState extends State<CustomExpansionTile> {
+
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    List<MusicNote> _musicNotes = widget.song.notes;
+
+    return ExpansionTile(
+      title: Container(
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            widget.song.songName,
+            style: TextStyle(
+              fontSize: 24.0, color: isExpanded ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+        // Change header (which is a Container widget in this case) background colour here.
+        color: Colors.transparent,
+      ),
+      leading: Icon(
+        Icons.music_note,
+        color: isExpanded ? Colors.white : Colors.black,
+        size: 36.0,
+      ),
+      trailing: isExpanded ? Icon(
+        Icons.arrow_drop_up,
+        color: Colors.white) :
+          Icon(
+            Icons.arrow_drop_down,
+            color: Colors.black,
+      ),
+      children:
+      <Widget>[
+        MaterialButton(
+          minWidth: 100,
+          color: Colors.red,
+          splashColor: Colors.orange,
+          onPressed: () async {
+            if (await canLaunch(widget.song.youtubeUrl)) {
+              launch(widget.song.youtubeUrl);
+            };
+          },
+          child: Text("Youtube"),
+        ),
+        Column(
+          children: _musicNotes.map((notes) => MusicButton(musicNote: notes)).toList(),
+        ),
+      ],
+      backgroundColor: Colors.transparent,
+      onExpansionChanged: (bool expanding) => setState(() => this.isExpanded = expanding),
+    );
+  }
+}
+
